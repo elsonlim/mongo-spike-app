27a4e1abe22a66abfa428bf76f3387d47ce70057
Elysia Ong <elysia.owx@gmail.com>
2019-05-01 22:20:18 +0800

    Implement search by address

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,8 @@ class Header extends React.Component {
 
         this.state = {
             inputLat: this.props.lat,
-            inputLng: this.props.lng
+            inputLng: this.props.lng,
+            address: ''
         };
     }
 
@@ -17,10 +18,23 @@ class Header extends React.Component {
                 inputLat: nextProps.lat,
                 inputLng: nextProps.lng
             });
+            // this.getAddress();
         }
     }
 
-    handleClick = () => {
+    handleAddressClick = () => {
+        const geocoder = new window.google.maps.Geocoder();
+        geocoder.geocode({"address": this.state.address}, (results, status) => {
+            if (status === "OK") {
+                this.props.setPosition(
+                    results[0].geometry.location.lat(),
+                    results[0].geometry.location.lng()
+                );
+            }
+        });
+    }
+
+    handlePositionClick = () => {
         this.props.setPosition(this.state.inputLat, this.state.inputLng);
     }
 
@@ -29,13 +43,25 @@ class Header extends React.Component {
             <div className="Header">
                 <div className="Title">Simple Markers</div>
                 <div className="Control">
-                    <label htmlFor="lat" className="Label">Latitude</label>
-                    <input id="lat" type="text" value={this.state.inputLat}
-                        onChange={(event) => this.setState({inputLat: Number.parseFloat(event.target.value)})} />
-                    <label htmlFor="lng" className="Label">Longitude</label>
-                    <input id="lng" type="text" value={this.state.inputLng}
-                        onChange={(event) => this.setState({inputLng: Number.parseFloat(event.target.value)})} />
-                    <button onClick={this.handleClick}>Search around me!</button>
+                    <div className="Control-item">
+                        <label htmlFor="address" className="Label">Address</label>
+                        <label htmlFor="lat" className="Label">Latitude</label>
+                    </div>
+                    <div className="Control-item">
+                        <input id="address" type="text" value={this.state.address} placeholder="Type in an address"
+                            onChange={(event) => this.setState({address: event.target.value})} />
+                        <div className="Control">
+                            <input id="lat" type="text" value={this.state.inputLat}
+                                onChange={(event) => this.setState({inputLat: Number.parseFloat(event.target.value)})} />
+                            <label htmlFor="lng" className="Label">Longitude</label>
+                            <input id="lng" type="text" value={this.state.inputLng}
+                                onChange={(event) => this.setState({inputLng: Number.parseFloat(event.target.value)})} />
+                        </div>
+                    </div>
+                    <div className="Control-item">
+                        <button onClick={this.handleAddressClick}>Search by Address!</button>
+                        <button onClick={this.handlePositionClick}>Search by Position!</button>
+                    </div>
                 </div>
             </div>
         )
